78ac7dc0356e94b546006a34f68f99e16586b857
ThanishInfotech <thanishinfotech@gmail.com>
2019-06-14 18:50:25 +0530

    sumthree1

---
+++ sumthree.java
@@ -0,0 +1,14 @@
+class sumthree
+{
+	public static void main(String[]args)
+	{
+		int a,b,c;
+		a=400;
+		b=200;
+		c=120;
+		c=a+b+c;
+		System.out.println("The Sum of three values are "+c);
+	}
+}
+
+
