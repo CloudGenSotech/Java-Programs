7d8c5e4d883e9b65091f0bc0a46ff02a4b9a0b5d
ThanishInfotech <thanishinfotech@gmail.com>
2019-07-03 12:01:52 +0530

    dd

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+class ifelse
+{
+        public static void main(String[] args)
+        {
+                int a,b;
+                a=50;
+                b=30;
+                if(a>b)
+                        System.out.println("A is big"+a);
+                else
+                        System.out.println("B is Big: "+c);
+        }
+}
